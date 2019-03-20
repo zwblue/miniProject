020d9bc7f70e94123cdadf38bec661de8724722d
左旺 <zuowang@zuowangdeMacBook-Pro.local>
2019-03-20 17:26:52 +0800

    更新日志的页面功能初步完成

--- EHTCminiProject/src/pages/updateLog/updateLog.tsx
+++ EHTCminiProject/src/pages/updateLog/updateLog.tsx
@@ -9,16 +9,73 @@ type PageOwnProps = {}
 
 type PageState = {}
 
-interface Index {
+interface updateLog {
   props: PageOwnProps;
 }
 
-class Index extends Component {
+class updateLog extends Component {
   
     config: Config = {
     navigationBarTitleText: '首页'
   }
-
+  state={
+    logList: [
+      {
+        title: 'e海通财小程序v1.3.3',
+        desc: '更新内容',
+        contentArray: ['为小程序添加置顶行情信息'],
+        time: '2017年11月'
+      },
+      {
+        title: 'e海通财小程序v1.3.2',
+        desc: '更新内容',
+        contentArray: ['修改美股指数等分时图显示不正常问题', '修改html解析问题'],
+        time: '2017年9月'
+      },
+      {
+        title: 'e海通财小程序v1.3.1',
+        desc: '更新内容',
+        contentArray: [
+          '首页tab增加资讯功能',
+          '行情“沪深”中增加资金流入、资金流出模块',
+          '行情增加“全球”模块',
+          '行情增加“全部”模块',
+          '个股分时线增加小红点',
+          '个股页面增加“资金”模块',
+          '个股页面增加“诊股”模块',
+          '“我的”点击二维码增加版本修改信息'
+        ],
+        time: '2017年7月'
+      },
+      {
+        title: 'e海通财小程序v1.2.5',
+        desc: '更新内容',
+        contentArray: ['个股十字线重叠问题', '修改其他已知问题'],
+        time: '2017年6月1日'
+      },
+      {
+        title: 'e海通财小程序v1.2.4',
+        desc: '更新内容',
+        contentArray: [
+          '首页排版布局修改',
+          '首页指数样式修改',
+          '新增4小时新闻直播模块，以及更多详情页',
+          '新增我的自选模块',
+          '新增入口板块、港股',
+          '新增自选资讯功能',
+          '新增编辑自选股功能',
+          '个股页面新增十字线功能',
+          '个股页面新增资讯模块',
+          '个股页面新增大事件模块',
+          '个股页面新增F10模块',
+          '修改个股页面添加自选按钮位置',
+          '修改个股页面分钟K线下拉菜单',
+          '修改其他已知问题'
+        ],
+        time: '2017年5月31日'
+      }
+    ]
+  }
   componentWillReceiveProps (nextProps) {
     console.log(this.props, nextProps)
   }
@@ -30,9 +87,30 @@ class Index extends Component {
   componentDidHide () { }
 
   render () {
+    const {logList} = this.state
     return (
-      <View className='index'>
-        <Text>updateLog</Text>
+      <View className='log-box'>
+        <View className='newest'>最新版本</View>
+        {logList.map((item, index)=>{
+          return (
+            <View key={index}>
+              <View className="title">
+                <View className="lf">{item.title}</View>
+                <View className="rt">{item.time}</View>
+              </View>
+              <View className="content">
+                <View className="desc">{item.desc}</View>
+                {item.contentArray.map((item, contentIndex)=>{
+                 return(
+                  <View key={contentIndex}>
+                    {(contentIndex + 1) + '.' + item}
+                  </View>
+                 )
+                })}
+              </View>
+            </View>
+          )
+        })}
       </View>
     )
   }
@@ -45,4 +123,4 @@ class Index extends Component {
 //
 // #endregion
 
-export default Index as ComponentClass<PageOwnProps, PageState>
+export default updateLog as ComponentClass<PageOwnProps, PageState>
--- EHTCminiProject/src/pages/user/user.tsx
+++ EHTCminiProject/src/pages/user/user.tsx
@@ -1,7 +1,7 @@
 import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
-
+import { View, Text, Image } from '@tarojs/components'
+import ht_code from '../../images/user/ht_code.png'
 import './user.scss'
 
 
@@ -18,21 +18,56 @@ class Index extends Component {
     config: Config = {
     navigationBarTitleText: '首页'
   }
-
+  state = {
+    timer: 1500,
+    animationData: []
+  }
   componentWillReceiveProps (nextProps) {
     console.log(this.props, nextProps)
   }
 
   componentWillUnmount () { }
 
-  componentDidShow () { }
+  componentDidShow () {
 
-  componentDidHide () { }
+   }
 
+  componentDidHide () { 
+    this.rotatePoint(0, 0)
+  }
+  rotatePoint (timer, deg) {
+    const animation = Taro.createAnimation({
+      duration: timer,
+      timingFunction: 'ease'
+    })
+    animation.export()
+    animation.rotate(deg).step()
+    this.setState({
+      animationData: animation.export()
+    })
+  }
+  jumpToPage = () => {
+    // 旋转动画
+    this.rotatePoint(this.state.timer, 1440)
+    setTimeout(() => {
+      Taro.navigateTo({
+        url: `/pages/updateLog/updateLog`
+      })
+      // 小程序中返回时并没有将数据重置，所以手动将其归0，否则将会出现不转的情况
+    }, this.state.timer)
+  }
   render () {
     return (
-      <View className='index'>
-        <Text>User</Text>
+      <View className='user-box'>
+        <View className="logo-box" onClick={this.jumpToPage}>
+          <Image className='ht-image' src={ht_code}></Image>
+          <View className='logo' animation={this.state.animationData}></View>
+        </View>
+        <View className='desc'>
+          <Text>下载海通APP，</Text>        
+          <Text>查看行情、快速交易，一站式搞定。</Text>        
+          <Text>还可在线开户，助您一指擒牛！</Text>        
+        </View>
       </View>
     )
   }
